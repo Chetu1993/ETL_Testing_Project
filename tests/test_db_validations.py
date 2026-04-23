631bba85a533f97deadab26db6eac7c661757a7c
chetu1993 <schetankumar123@gmail.com>
2026-04-23 18:57:42 +0530

    updated files

---
+++ tests/test_db_validations.py
@@ -0,0 +1,47 @@
+import pandas as pd
+
+from etl_testing.db_validations import (
+row_count_check,
+target_null_count,
+data_validation_check,
+schema_validation_check,
+duplicate_validation_check,
+datatype_check_validation,
+salary_range_validation
+)
+
+def get_source_data():
+    return pd.DataFrame({"id":[1,2,3],"salary":[1000,2000,3000]})
+
+def get_target_data():
+    return pd.DataFrame({"id":[1,2,3],"salary_bonus":[1200,2400,3600]})
+
+
+def test_row_count():
+    res=row_count_check(get_source_data(),get_target_data())
+    assert res["status"]=='PASS'
+
+def test_null_check():
+    res=target_null_count(get_target_data())
+    assert res["status"]=="PASS"
+
+def test_data_validation():
+    res=data_validation_check(get_source_data(),get_target_data())
+    assert res["status"]=='PASS'
+
+def test_schema():
+    res=schema_validation_check(get_source_data(),get_target_data())
+    assert res["status"]=='FAIL'
+
+def test_duplicates():
+    df=get_target_data()
+    res=duplicate_validation_check(df)
+    assert res["status"]=='PASS'
+
+def test_datatype():
+    res=datatype_check_validation(get_source_data(),get_target_data())
+    assert res["status"]==('FAIL')
+
+def test_salary_range():
+    res=salary_range_validation(get_target_data())
+    assert res["status"]=='PASS'
